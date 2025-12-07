06a3a53b4cb9dc5bf92485c3ae87c1a326a41022
김나경 <1030n@naver.com>
2025-12-07 19:22:43 +0900

    :wrench: Settings: SecurityConfig 경로 수정

--- src/main/java/com/wit/payment/global/config/SecurityConfig.java
+++ src/main/java/com/wit/payment/global/config/SecurityConfig.java
@@ -1,10 +1,11 @@
-/* 
- * Copyright (c) WIT Global 
+/*
+ * Copyright (c) WIT Global
  */
 package com.wit.payment.global.config;
 
+import com.wit.payment.global.security.JwtAuthenticationFilter;
 import jakarta.servlet.http.HttpServletResponse;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,10 +16,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.wit.payment.global.security.JwtAuthenticationFilter;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -92,7 +89,7 @@ public class SecurityConfig {
                     .permitAll()
 
                     // 웹용 API
-                    .requestMatchers("/api/pay/phone", "api/pay/options")
+                    .requestMatchers("/api/pay/phone", "/api/pay/options")
                     .permitAll()
                     .requestMatchers("/api/admin/**")
                     .hasRole("ADMIN")
