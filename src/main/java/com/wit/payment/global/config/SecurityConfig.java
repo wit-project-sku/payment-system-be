eac50025d63ad3a07922d0478a94a253a5e10676
김나경 <1030n@naver.com>
2025-12-08 23:07:54 +0900

    :wrench: Settings: pay 관련 api 인증인가 허용

--- src/main/java/com/wit/payment/global/config/SecurityConfig.java
+++ src/main/java/com/wit/payment/global/config/SecurityConfig.java
@@ -1,10 +1,11 @@
-/* 
- * Copyright (c) WIT Global 
+/*
+ * Copyright (c) WIT Global
  */
 package com.wit.payment.global.config;
 
+import com.wit.payment.global.security.JwtAuthenticationFilter;
 import jakarta.servlet.http.HttpServletResponse;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,10 +16,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.wit.payment.global.security.JwtAuthenticationFilter;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -88,7 +85,7 @@ public class SecurityConfig {
                         "/api/categories",
                         "/api/categories/first",
                         "/api/tl3800/*",
-                        "/api/pay")
+                        "/api/pay/*")
                     .permitAll()
 
                     // 로컬 결제 서버 → 중앙 서버용 내부 콜 (성공/실패/취소 보고)
