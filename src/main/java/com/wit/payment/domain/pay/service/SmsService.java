289403dd12389c4e2c89dae205a137464e7e4857
김나경 <1030n@naver.com>
2025-12-09 16:09:57 +0900

    :recycle: Refactor: sms 멘트 변경

--- src/main/java/com/wit/payment/domain/pay/service/SmsService.java
+++ src/main/java/com/wit/payment/domain/pay/service/SmsService.java
@@ -1,8 +1,10 @@
-/* 
- * Copyright (c) WIT Global 
+/*
+ * Copyright (c) WIT Global
  */
 package com.wit.payment.domain.pay.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -12,9 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
@@ -42,11 +41,9 @@ public class SmsService {
     params.add("receiver", receiver);
     params.add(
         "msg",
-        "안녕하세요, 고객님.\n"
-            + "위트테리아를 이용해주셔서 감사합니다.\n\n"
-            + "배송을 위해 아래 링크에서\n"
-            + "필수 정보 및 배송 주소를 입력해주세요.\n\n"
-            + "https://witteria.com/mobile/search");
+        "[위트테리아]\n"
+            + "상품 수령을 위한 배송 정보를 입력해주세요\n\n"
+            + "https://unijuni.store/mobile");
     params.add("msg_type", "SMS");
 
     HttpHeaders headers = new HttpHeaders();
